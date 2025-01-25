ef0711b74a746a0da9bf0b288e8d8ed9c864e35b
James King <james.king@facepunchstudios.com>
2025-01-25 17:02:49 +0000

    Basic demo of a .movie resource

--- Libraries/moviemaker/Code/MovieClip/MovieFile.cs
+++ Libraries/moviemaker/Code/MovieClip/MovieFile.cs
@@ -5,10 +5,12 @@
 /// <summary>
 /// A <see cref="MovieClip"/> stored as a resource.
 /// </summary>
+[GameResource( "Movie Clip", "movie", $"A movie clip created with the {nameof(MoviePlayer)} component.", Icon = "movie" )]
 public sealed class MovieFile : GameResource
 {
 	/// <summary>
 	/// The movie clip stored in this resource.
 	/// </summary>
+	[Hide]
 	public MovieClip? Clip { get; set; }
 }
